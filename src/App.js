12c2623e0558e82e7b213061e3f5ab10cf56f504
Renka2414 <luchyn.ira@gmail.com>
2024-11-20 11:44:50 +0200

    Ira profile div added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import "./App.css";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import IraProfile from "./components/IraProfile";
+
+/*function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Body />
+    </div>
+  );
+}
+
+export default App;*/
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Body />
+      <div className="body-container">
+      <IraProfile />
+      </div>
     </div>
   );
 }
