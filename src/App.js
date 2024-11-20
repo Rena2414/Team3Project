ba42dbfa911c17585d3d3f0fbd1fd177f2e6baa8
Yaroslav Milchenko <zadirakalena@gmail.com>
2024-11-20 12:59:46 +0200

    Changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import "./App.css";
 import Header from "./components/Header";
-import Body from "./components/Body";
+import AlinaProfile from "./components/AlinaProfile";
+
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Body />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <div className="body-container">
+                <AlinaProfile />
+            </div>
+        </div>
+    );
 }
 
 export default App;
