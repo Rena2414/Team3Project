2cbcc0f7eb09811b9500746e7ce1836ac7d9de9e
NeXuSs-69 <mmmalets@gmail.com>
2024-11-20 02:36:36 +0200

    надіюсь останній коміт

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,7 +4,7 @@ const Body = ({ name = "John Doe", position = "Software Engineer", photoSrc = "/
   return (
     <main style={styles.body}>
       <div style={styles.card}>
-        <img src={"/avatar-default.png"} alt="John Doe" style={styles.photo} />
+        <img src={photoSrc} alt={name} style={styles.photo} />
         <div style={styles.textContainer}>
           <h2 style={styles.name}>{name}</h2>
           <p style={styles.position}>{position}</p>
