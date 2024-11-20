2fb757bf64880d7c6adaf3af0547fe142e61fe4d
Renka2414 <luchyn.ira@gmail.com>
2024-11-20 11:51:58 +0200

    Adding IraProfile div to the site

--- src/App.js
+++ src/App.js
@@ -3,16 +3,6 @@ import Header from "./components/Header";
 import Body from "./components/Body";
 import IraProfile from "./components/IraProfile";
 
-/*function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Body />
-    </div>
-  );
-}
-
-export default App;*/
 
 function App() {
   return (
