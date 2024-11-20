f385d7a9d15bae35b0770b97ab845a8f033e13e5
NeXuSs-69 <mmmalets@gmail.com>
2024-11-20 02:36:36 +0200

    надіюсь останній коміт

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import "./App.css";
 import Header from "./components/Header";
-import Body from "./components/Body";
+import MykhailoProfile from "./components/MykhailoProfile";
+
+// Короче, створюєте в папці components файл з назвою Ім'яProfile.js, код можете взяти з мене (MykhailoProfile.js), тоді мінєте 
+// основні параметри, фото для ави завантажуєте в папку public, має бути кругла пнгшка, я юзав цей сайт для обрізання - https://crop-circle.imageonline.co/
+// тоді зверху цього файлу(App.js) вставляєте import ім'яProfile from "./components/ім'яProfile";
+// і основне, знизу в App() треба вставити <ім'яProfile />
+
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Body />
+      <div className="body-container">
+      <MykhailoProfile />
+      </div>
     </div>
   );
 }
