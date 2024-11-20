9d61bda4359d38e2ba643122972cb7d0a466098c
Yaroslav Milchenko <zadirakalena@gmail.com>
2024-11-20 12:16:25 +0200

    Changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import "./App.css";
 import Header from "./components/Header";
-import Body from "./components/Body";
+import YaroslavProfile from "./components/YaroslavProfile";
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Body />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <div className="body-container">
+                <YaroslavProfile />
+            </div>
+        </div>
+    );
 }
 
 export default App;
